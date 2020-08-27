453610986fb5fb9b7e4588894d12c78123b575a2
JoGeun <40857478+gusrmsdlrh@users.noreply.github.com>
2020-08-27 14:20:29 +0900

    Update and rename UAIS.py to UAIS_ver1.py

--- UAIS_ver1.py
+++ UAIS_ver1.py
@@ -5,17 +5,15 @@ import socket
 import struct
 from timeit import default_timer as timer
 import binascii
-
+ 
 def primary_1():
         print("===========PRIMARY SCAN===========")
         MCAST_GRP = '239.255.255.250'
         MCAST_PORT = 1900
-
+ 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(time)
         sock.sendto(primary_pkt, (MCAST_GRP, MCAST_PORT))
-        mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
         try:
                 while 1:
                         data, address = sock.recvfrom(1024)
@@ -24,54 +22,54 @@ def primary_1():
                                 location_data = re.search(b'(?m)(http://.*)', data, re.I).group()
                                 location_url = location_data.split(b'\r')[0]
                                 resp = request.get(location_url)
-
+ 
                                 # XML Tags extract
                                 model_description = scrape(resp.text, '<modelDescription>', '</modelDescription>')
                                 device_type = scrape(resp.text, '<deviceType>', '</deviceType>')
                                 friendly_name = scrape(resp.text, '<friendlyName>', '</friendlyName>')
                                 tags_data = str(device_type) + " " + str(model_description) + " " + str(friendly_name)
-
+ 
                                 # keyword match
                                 keyword_match(tags_data)
         except socket.timeout as timeerror:
                 print ("Primary Scan 1 : " + str(timeerror))
                 primary_str = primary_2()
-
+ 
 def primary_2():
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(time)
-
+ 
         try:
                 sock.connect((target, 1900))
                 sock.send(primary_pkt)
                 data = sock.recv(1024)
-
+ 
                 # LOCATION : XXX
                 location_data = re.search(b'(?m)(http://.*)', data, re.I).group()
                 location_url = location_data.split(b'\r')[0]
                 resp = request.get(location_url)
-
+ 
                 # XML Tags extract
                 model_description = scrape(resp.text, '<modelDescription>', '</modelDescription>')
                 device_type = scrape(resp.text, '<deviceType>', '</deviceType>')
                 friendly_name = scrape(resp.text, '<friendlyName>', '</friendlyName>')
                 tags_data = str(device_type) + " " + str(model_description) + " " + str(friendly_name)
-
+ 
                 # keyword match
                 keyword_match(tags_data)
         except socket.timeout as timeerror:
                 print ("Primary Scan 2 : " + str(timeerror))
         except socket.error as err:
                 print ("Primary Scan 2 : " + str(err))
-
+ 
 #Data extraction
 def scrape(text, start_trig, end_trig):
         if text.find(start_trig) != -1:
                 return text.split(start_trig, 1)[-1].split(end_trig, 1)[0]
         else:
                 return False
-
-
+ 
+ 
 def Auxiliary_1():
         print("===========Auxiliary SCAN===========")
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -86,7 +84,7 @@ def Auxiliary_1():
         except socket.error as err:
                 print("Auxiliary Scan 1 " + str(err))
                 return "Null"
-
+ 
 def Auxiliary_2():
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.settimeout(time)
@@ -102,11 +100,11 @@ def Auxiliary_2():
                 print("Auxiliary Scan 2 " + str(err))
                 Auxiliary_3_str = Auxiliary_3()
                 return Auxiliary_3_str
-
+ 
 def Auxiliary_3():
         MCAST_GRP = '224.0.0.251'
         MCAST_PORT = 5353
-
+ 
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind(('', MCAST_PORT))
@@ -122,7 +120,7 @@ def Auxiliary_3():
         except socket.timeout as timeerror:
                 print ("Auxiliary Scan 3 "+str(timeerror))
                 return "Null"
-
+ 
 def Auxiliary_reverse_ip(ip_addr):
         lens = []
         reverse = (ip_addr.split('.'))
@@ -132,7 +130,7 @@ def Auxiliary_reverse_ip(ip_addr):
         var_2 = binascii.unhexlify('0' + str(lens[2]))
         var_1 = binascii.unhexlify('0' + str(lens[1]))
         var_0 = binascii.unhexlify('0' + str(lens[0]))
-
+ 
         Auxiliary_2_pkt = b'\x00\x00\x01\x00\x00\x0e\x00\x00\x00\x00\x00\x00' \
                       b'\x05_http\x04_tcp\x05local\x00\x00\x0c\x00\x01' \
                       b'\x05_rtsp\x04_tcp\x05local\x00\x00\x0c\x00\x01' \
@@ -151,42 +149,42 @@ def Auxiliary_reverse_ip(ip_addr):
                           1].encode() + var_0 + reverse[0].encode() + \
                       b'\x07in-addr\x04arpa\x00\x00\x0c\x00\x01'
         return Auxiliary_2_pkt
-
+ 
 def keyword_match(data):
         # Types of IoT Devices
         '''
-        o	Yang, K., Li, Q., & Sun, L. (2019). Towards automatic fingerprinting of IoT devices in the cyberspace. Computer Networks, 148, 318-327.
-        o	Meidan, Y., Bohadana, M., Shabtai, A., Guarnizo, J. D., Ochoa, M., Tippenhauer, N. O., & Elovici, Y. (2017, April). ProfilIoT: a machine learning approach for IoT device identification based on network traffic analysis. In Proceedings of the symposium on applied computing (pp. 506-509).
-        o	Sivanathan, A., Sherratt, D., Gharakheili, H. H., Radford, A., Wijenayake, C., Vishwanath, A., & Sivaraman, V. (2017, May). Characterizing and classifying IoT traffic in smart cities and campuses. In 2017 IEEE Conference on Computer Communications Workshops (INFOCOM WKSHPS) (pp. 559-564). IEEE.
-        o	Kawai, H., Ata, S., Nakamura, N., & Oka, I. (2017, November). Identification of communication devices from analysis of traffic patterns. In 2017 13th International Conference on Network and Service Management (CNSM) (pp. 1-5). IEEE.
-        o	Meidan, Y., Bohadana, M., Shabtai, A., Ochoa, M., Tippenhauer, N. O., Guarnizo, J. D., & Elovici, Y. (2017). Detection of unauthorized iot devices using machine learning techniques. arXiv preprint arXiv:1709.04647.
-
-        •	IoT device categories (Total 20)
-                o	Router / Switch / Hub / Gateway / Modem / (Wireless) access point
-                o	(IP) television / TV
-                o	Programmable / (Logic) controller
-                o	Sensor / Thermostat
-                o	PC / Laptop
-                o	(IP / Network) camera
-                o	NAS
-                o	(Digital) video
-                o	Trigger
-                o	Recorder
-                o	Printer
-                o	Socket
-                o	Firewall
-                o	Refrigerator
-                o	Monitor
-                o	(Smart) watch
-                o	Smartphone
-                o	Healthcare (device)
-                o	Digital media receiver
-                o	Consumer game
+        o    Yang, K., Li, Q., & Sun, L. (2019). Towards automatic fingerprinting of IoT devices in the cyberspace. Computer Networks, 148, 318-327.
+        o    Meidan, Y., Bohadana, M., Shabtai, A., Guarnizo, J. D., Ochoa, M., Tippenhauer, N. O., & Elovici, Y. (2017, April). ProfilIoT: a machine learning approach for IoT device identification based on network traffic analysis. In Proceedings of the symposium on applied computing (pp. 506-509).
+        o    Sivanathan, A., Sherratt, D., Gharakheili, H. H., Radford, A., Wijenayake, C., Vishwanath, A., & Sivaraman, V. (2017, May). Characterizing and classifying IoT traffic in smart cities and campuses. In 2017 IEEE Conference on Computer Communications Workshops (INFOCOM WKSHPS) (pp. 559-564). IEEE.
+        o    Kawai, H., Ata, S., Nakamura, N., & Oka, I. (2017, November). Identification of communication devices from analysis of traffic patterns. In 2017 13th International Conference on Network and Service Management (CNSM) (pp. 1-5). IEEE.
+        o    Meidan, Y., Bohadana, M., Shabtai, A., Ochoa, M., Tippenhauer, N. O., Guarnizo, J. D., & Elovici, Y. (2017). Detection of unauthorized iot devices using machine learning techniques. arXiv preprint arXiv:1709.04647.
+ 
+        •    IoT device categories (Total 20)
+                o    Router / Switch / Hub / Gateway / Modem / (Wireless) access point
+                o    (IP) television / TV
+                o    Programmable / (Logic) controller
+                o    Sensor / Thermostat
+                o    PC / Laptop
+                o    (IP / Network) camera
+                o    NAS
+                o    (Digital) video
+                o    Trigger
+                o    Recorder
+                o    Printer
+                o    Socket
+                o    Firewall
+                o    Refrigerator
+                o    Monitor
+                o    (Smart) watch
+                o    Smartphone
+                o    Healthcare (device)
+                o    Digital media receiver
+                o    Consumer game
         '''
         # Null data
         if data == "NullNull":
             sys.exit()
-
+ 
         DT_list=[['router','switch','hub','gateway','modem', 'access point', 'accesspoint'],
                 ['television', 'tv'],['programmable', 'controller'],['sensor', 'thermostat'],
                 ['pc', 'laptop'],['camera'],['nas'],['video'],['trigger'],
@@ -199,21 +197,21 @@ def keyword_match(data):
                 'printer','socket','firewall','refrigerator',
                 'monitor','watch','smartphone','healthcare',
                 'digital','game']
-
+ 
         # Type Var Declaration
         for i in DT:
                 globals()['{}_count'.format(i)]=0
-
+ 
         # UnboundLocalError Exception
         global router_count, tv_count, controller_count, sensor_count, laptop_count, camera_count, nas_count, video_count, \
                    trigger_count, recorder_count, printer_count, socket_count, firewall_count, refrigerator_count, monitor_count, \
                    watch_count, smartphone_count, healthcare_count, digital_count, game_count
-
+ 
         # Device Type Count List
         list_ls = [router_count, tv_count, controller_count, sensor_count, laptop_count, camera_count, nas_count, video_count, \
                    trigger_count, recorder_count, printer_count, socket_count, firewall_count, refrigerator_count, monitor_count, \
                    watch_count, smartphone_count, healthcare_count, digital_count, game_count]
-
+ 
         # Device Type Count
         for j in range(20):
                 for i in DT_list[j]:
@@ -229,7 +227,7 @@ def keyword_match(data):
                         max_idx = i
                 elif maxValue != 0 and maxValue == list_ls[i]:
                         second_idx = i
-
+ 
         # Device Type Result
         if maxValue == 0:
                 print ("Data : " + data)
@@ -244,30 +242,33 @@ def keyword_match(data):
                 DT_class=timer()
                 print("Time Stamp ---> " + str(DT_class - start) + "\r\n")
                 sys.exit()
-
+ 
 if __name__=="__main__":
         # Time Stamp
         start = timer()
-
+ 
         primary_pkt = b"M-SEARCH * HTTP/1.1\r\n" \
                    b"HOST: 239.255.255.250:1900\r\n" \
                    b"MAN: \"ssdp:discover\"\r\n" \
                    b"MX: 1\r\n" \
                    b"ST: upnp:rootdevice\r\n\r\n"
-
+ 
         auxiliary_pkt = b"\x82\x28\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00" \
                     b"\x20\x43\x4b\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41" \
                     b"\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x00\x00\x21\x00\x01"
-
+ 
         time=0.2
         request = requests.Session()
         target=sys.argv[1]
         Auxiliary_2_pkt=Auxiliary_reverse_ip(target)
-
+ 
         primary_1()
         auxiliary_1_string=Auxiliary_1()
         auxiliary_2_string=Auxiliary_2()
         keyword_match(auxiliary_1_string+auxiliary_2_string)
-
+ 
         end = timer()
         print("Time Stamp ---> " + str(end - start)+"\r\n")
+
+
+출처: https://johyungen.tistory.com/503 [Self-Improvement]
